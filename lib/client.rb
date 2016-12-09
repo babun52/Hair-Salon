fb7eafaa6112ea5e075205b8f3244ad3ff143c8e
James Lee <babun5209@gmail.com>
2016-12-09 10:57:42 -0800

    Add .all(class method).

--- lib/client.rb
+++ lib/client.rb
@@ -6,4 +6,16 @@ class Client
       @id = attributes.fetch(:id)
       @stylist_id = attributes.fetch(:stylist_id)
   end
+
+  define_singleton_method(:all) do
+    returned_clients = DB.exec("SELECT * FROM clients;")
+    clients = []
+    returned_clients.each do |client|
+      name = client.fetch('name')
+      id = client.fetch('id').to_i
+      stylist_id = client.fetch('stylist_id').to_i
+      clients.push(Client.new({:name => name, :id => id, :stylist_id => stylist_id}))
+    end
+    clients
+  end
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,11 +1,25 @@
 require('rspec')
+require('pg')
 require('client')
 
+DB = PG.connect({:dbname => 'hair_salon'})
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM clients *;")
+  end
+end
+
 describe(Client) do
-  describe('#initialize') do
-    it('will return a new client with name and id') do
-      new_client = Client.new({:name => "James", :id => nil, :stylist_id => 1})
+  describe('#name') do
+    it('will return a new client with name') do
+      new_client = Client.new({:name => "James", :id => nil, :stylist_id => nil})
       expect(new_client.name()).to(eq("James"))
+    end
+  end
+  describe('.all') do
+    it('is empty at first') do
+      expect(Client.all()).to(eq([]))
     end
   end
 end
